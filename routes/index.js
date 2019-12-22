c6f0e0a33c5252b9471ba62e9d14c00287f2e751
Dương Hoàng Son <48278847+hoangson26021999@users.noreply.github.com>
2019-12-22 12:30:21 +0700

    Add student view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Express' });
+});
+
+router.get("/home", function(request, response)  {
+
+  response.render("student/home");
+});
+router.get("/registry", function(request, response)  {
+
+  response.render("student/registry");
+});
+router.get("/reset-password", function(request, response)  {
+
+  response.render("student/reset-password");
+});
+router.get("/council", function(request, response)  {
+
+  response.render("faculty/council");
 });
 router.post('/', (req, res, next) => {
   let sql = "select * from user";
